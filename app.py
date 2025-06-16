a48f3399e290a56857f8fb6aed909a3f3c168a2c
TIRCIS Spin Off - Lhorie <lhorie.pirnay@unamur.be>
2025-06-16 14:07:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,8 @@ st.markdown("Bienvenue sur votre espace TIRCIS.")
 page = st.sidebar.selectbox("Navigation", ["Accueil", "Graphe de Causalité", "Dashboard de KPIs"])
 
 # Sidebar
-#st.sidebar.image("image/logo.png", width=150)
-st.sidebar.subheader("About This App")
-st.sidebar.info(
-    """
-    Cette application permet de visualiser les liens de causalité entre les indicateurs de performance (KPIs) 
-    d'une organisation. 
+st.sidebar.image("image/logo.png", width=150)
 
-    Le graphe représente les KPIs comme des nœuds, reliés entre eux par des flèches indiquant des relations causales 
-    identifiées statistiquement.
-
-    Utilisez les filtres pour explorer les indicateurs par catégorie BSC, durabilité, ou poids du lien.
-    """
-)
 
 
 
