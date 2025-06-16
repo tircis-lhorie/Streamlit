1948cabcbfd020de359c82677e72941da0dc00b6
TIRCIS Spin Off - Lhorie <lhorie.pirnay@unamur.be>
2025-06-16 13:45:59 +0200

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
 
+st.title("Dashboard de KPIs")
+st.markdown("Explorez les KPIs de votre entreprise !")
