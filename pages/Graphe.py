6aabd5b9cea03c0a6444bbf15cb1314e56c0f644
TIRCIS Spin Off - Lhorie <lhorie.pirnay@unamur.be>
2025-06-16 13:37:56 +0200

    Update Graphe.py

--- pages/Graphe.py
+++ pages/Graphe.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
 
+st.title("Graphe de causalité")
+st.markdown("Explorez les effets de causalités entre les KPIs de votre entreprise !")
