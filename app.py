48841623dd7bba5a4024d24bc9a64c5303cd8ae0
TIRCIS Spin Off - Lhorie <lhorie.pirnay@unamur.be>
2025-06-16 14:16:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,22 +10,4 @@ st.set_page_config(page_title="TIRCIS Dashboard", layout="wide")
 st.title("Bienvenue")
 st.markdown("Bienvenue sur votre espace TIRCIS.")
 
-# Afficher le logo dans le coin supérieur gauche
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"] > div:first-child {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 st.sidebar.image("image/logo.png", use_container_width=True)
-
-
-page = st.sidebar.selectbox("Navigation", ["Accueil", "Graphe de Causalité", "Dashboard de KPIs"])
-
