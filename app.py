29434aeae19313745cd96ead6b060dd0a79f4242
TIRCIS Spin Off - Lhorie <lhorie.pirnay@unamur.be>
2025-06-17 10:55:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from streamlit_extras.switch_page_button import switch_page
 
 # --- Interface Streamlit ---
 st.set_page_config(page_title="TIRCIS Dashboard", layout="wide")
+st.sidebar.video("image/GIF.gif", use_container_width=True)
 st.sidebar.image("image/logo.png", use_container_width=True)
 
 st.sidebar.subheader("À propos de TIRCIS")
