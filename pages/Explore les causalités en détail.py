85b92d4bc0c5478a1d0f99f10410430ef54f7a3c
TIRCIS Spin Off - Lhorie <lhorie.pirnay@unamur.be>
2025-07-18 09:59:38 +0200

    Update Explore les causalités en détail.py

--- pages/Explore les causalités en détail.py
+++ pages/Explore les causalités en détail.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+# Si l'utilisateur n'est pas authentifié
+if "authenticated" not in st.session_state or not st.session_state.authenticated:
+    st.warning("Vous devez être connecté pour accéder à cette page.")
+    
+    if st.button("Retour à la page de connexion"):
+        st.switch_page("app.py") 
+    st.stop()
+
 st.set_page_config(page_title="TIRCIS Dashboard",
                    page_icon="image/icon-transparent.png",
                    layout="wide")
